6fa60d0d78b0325327b1be35a22521b9503bbb89
YoannPitance <pittyo88@gmail.com>
2023-08-04 16:48:56 +0200

    3- début page login

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import "./globals.scss";
+
 export const metadata = {
   title: "LaBelVita",
   description: "Maison de qualité en ménage et repassage",
@@ -6,6 +8,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
--- app/login/page.js
+++ app/login/page.js
@@ -1,3 +1,54 @@
+import Image from "next/image";
+import Link from "next/link";
+import "./login.module.scss";
+
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <>
+      <div className="logoTitle">
+        <Image src="/Logo1.png" width={219} height={71} alt="" />
+      </div>
+      <div className="vector4">
+        <Image src="/Vector4.png" width={571} height={404} alt="" />
+      </div>
+      <section id="connexion">
+        <div className="titleConnexion">
+          <h2>Connexion à votre espace</h2>
+          <h3>Bienvenue "User"</h3>
+        </div>
+        <div className="inputConnexion">
+          <input type="text" placeholder="Votre adresse email" />
+          <input type="text" placeholder="Mot de passe" />
+        </div>
+        <div className="forgottenPwrd">
+          <Link href="#">Mot de passe oublié ?</Link>
+        </div>
+        <div className="connexionBtn">
+          <button type="admit">Connexion</button>
+        </div>
+      </section>
+      <section id="socialNetworkConnexion">
+        <p>Ou...</p>
+        <div className="group1">
+          <ul>
+            <li>
+              <Link href="#"></Link>
+            </li>
+            <li>
+              <Link href="#"></Link>
+            </li>
+            <li>
+              <Link href="#"></Link>
+            </li>
+          </ul>
+        </div>
+        <div className="newAccount">
+          <span>Vous n'êtes pas inscrits ?</span>
+          <span>
+            <Link href="#">Créer un compte</Link>
+          </span>
+        </div>
+      </section>
+    </>
+  );
 }
