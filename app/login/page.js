9eaee4d7d833012882aecceb82fd4fc15fe4c91c
YoannPitance <pittyo88@gmail.com>
2023-08-05 12:20:21 +0200

    4- style page Login

--- app/login/page.js
+++ app/login/page.js
@@ -1,20 +1,32 @@
 import Image from "next/image";
 import Link from "next/link";
-import "./login.module.scss";
+import "./login.scss";
 
 export default function Login() {
   return (
     <>
-      <div className="logoTitle">
-        <Image src="/Logo1.png" width={219} height={71} alt="" />
-      </div>
       <div className="vector4">
-        <Image src="/Vector4.png" width={571} height={404} alt="" />
+        <Image
+          src="/Vector4.png"
+          width={240}
+          height={380}
+          alt="image vectorielle design"
+        />
       </div>
+      <section id="title">
+        <div className="logoTitle">
+          <Image
+            src="/Logo1.png"
+            width={219}
+            height={71}
+            alt="Logo LaBel'Vita"
+          />
+        </div>
+      </section>
       <section id="connexion">
         <div className="titleConnexion">
           <h2>Connexion à votre espace</h2>
-          <h3>Bienvenue "User"</h3>
+          <h3>Bienvenue "User"!</h3>
         </div>
         <div className="inputConnexion">
           <input type="text" placeholder="Votre adresse email" />
@@ -28,22 +40,37 @@ export default function Login() {
         </div>
       </section>
       <section id="socialNetworkConnexion">
-        <p>Ou...</p>
+        <div className="separation">
+          <span>
+            <img src="/Vector2.png" alt="trait de séparation" />
+          </span>
+          <span>Ou...</span>
+          <span>
+            <img src="/Vector2.png" alt="trait de séparation" />
+          </span>
+        </div>
+
         <div className="group1">
           <ul>
             <li>
-              <Link href="#"></Link>
+              <Link href="#">
+                <img src="/googlelogo.png" alt="Logo google" />
+              </Link>
             </li>
             <li>
-              <Link href="#"></Link>
+              <Link href="#">
+                <img src="/Groupfb.png" alt="Logo facebook" />
+              </Link>
             </li>
             <li>
-              <Link href="#"></Link>
+              <Link href="#">
+                <img src="/Vectorapple.png" alt="Logo Apple" />
+              </Link>
             </li>
           </ul>
         </div>
         <div className="newAccount">
-          <span>Vous n'êtes pas inscrits ?</span>
+          <span>Vous n'êtes pas inscrits ? </span>
           <span>
             <Link href="#">Créer un compte</Link>
           </span>
